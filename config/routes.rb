de0a6f36a880198a405ebefbadcdb47732ffab7d
Peter Jacobson <github email address>
2016-08-24 15:19:15 -0400

    angular

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
    namespace :api, defaults:{format: :json} do
     namespace :v1 do
-      resources :todos
+      resources :notes
     end
   end
 
-  root 'application#index'
+  root to: 'application#index'
 end
 
