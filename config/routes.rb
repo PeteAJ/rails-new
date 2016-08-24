7d04525a4c1bcc079ec2ee585d667e373f3b692d
Peter Jacobson <github email address>
2016-08-24 14:30:49 -0400

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root “application#index"
+ 
 
-  
+
    namespace :api, defaults:{format: :json} do
     namespace :v1 do
       resources :todos
     end
   end
+
+  root 'application#index'
 end
 
