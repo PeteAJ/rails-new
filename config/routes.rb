7c51e6f0116902dbcc973243d138798da09357be
Peter Jacobson <github email address>
2016-08-24 12:12:17 -0400

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-RailsBlog::Application.routes.draw do
-
-  resources :users
-  resources :tags
-  resources :posts
-
+Rails.application.routes.draw do
+  namespace :api, defaults:{format: :json} do
+    namespace :v1 do
+      resources :notes
+    end
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513204421) do
+ActiveRecord::Schema.define(version: 20160824160842) do
 
+  create_table "notes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
